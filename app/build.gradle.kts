74297e445cc155a790d4fe40987cca7a4b13f22f
tfs-apps-one <furu12080205@gmail.com>
2026-06-20 18:35:45 +0900

    初期リリース

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "tfsapps.shigadevelopment"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "tfsapps.shigadevelopment"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
